4f97dc3d97cc9ee5daa284f48a9dc32b6045e16b
Ludoo <ludoo0215@gmail.com>
2025-04-21 16:51:39 +0200

    Implement sqlite database system

--- src/bot.ts
+++ src/bot.ts
@@ -2,6 +2,8 @@
 import { Client, GatewayIntentBits, SlashCommandBuilder } from 'discord.js';
 import dotenv from 'dotenv';
 
+
+
 dotenv.config(); // lädt die .env-Datei
 
 // Client
@@ -9,10 +11,16 @@ const client = new Client({
   intents: [GatewayIntentBits.Guilds],
 });
 
+// Database
+const sqlite = require('sqlite3').verbose();
+let db = new sqlite.Database('./database.db', sqlite.OPEN_READWRITE | sqlite.OPEN_CREATE);
+
+
 // Online-Event
 client.once('ready', async () => {
   console.log(`${client.user?.tag} ist eingeloggt!`);
-//   Register Commands
+
+  //   Register Commands
   const data = new SlashCommandBuilder()
     .setName('ping')
     .setDescription('Antwortet mit Pong!');
@@ -25,6 +33,8 @@ client.once('ready', async () => {
   // Log registered commands
   console.log('Slash-Commands registriert:', commands.map(cmd => cmd.name).join(', '));
 
+  // Create Database Table
+  db.run(`CREATE TABLE IF NOT EXISTS incidents(id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT NOT NULL, description TEXT NOT NULL, messageid TEXT NOT NULL, status TEXT NOT NULL, created_at TIMESTAMP NOT NULL)`)
 });
 
 // Command-Handler
