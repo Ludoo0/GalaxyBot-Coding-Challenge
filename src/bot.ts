bd96bb7cf2f6270b21c4ba94b5a38968f4d9305e
Ludoo <ludoo0215@gmail.com>
2025-04-21 14:43:43 +0200

    Refactor comments in bot.ts for clarity and consistency

--- src/bot.ts
+++ src/bot.ts
@@ -1,14 +1,18 @@
+// Imports
 import { Client, GatewayIntentBits, SlashCommandBuilder } from 'discord.js';
 import dotenv from 'dotenv';
 
 dotenv.config(); // lädt die .env-Datei
 
+// Client
 const client = new Client({
   intents: [GatewayIntentBits.Guilds],
 });
 
+// Online-Event
 client.once('ready', async () => {
   console.log(`${client.user?.tag} ist eingeloggt!`);
+//   Register Commands
   const data = new SlashCommandBuilder()
     .setName('ping')
     .setDescription('Antwortet mit Pong!');
@@ -18,20 +22,22 @@ client.once('ready', async () => {
     console.error('Fehler beim Registrieren der Slash-Commands');
     return;
   }
-  // Log the registered commands to the console
+  // Log registered commands
   console.log('Slash-Commands registriert:', commands.map(cmd => cmd.name).join(', '));
 
 });
 
+// Command-Handler
 client.on('interactionCreate', async (interaction) => {
   if (!interaction.isCommand()) return;
 
   const { commandName } = interaction;
 
+//   Ping Command
   if (commandName === 'ping') {
     await interaction.reply('Pong!');
   }
 });
 
-
+// Login
 client.login(process.env.BOT_TOKEN);
